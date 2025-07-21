3ce29633bae41e0261b8b2280c96ccf3109de5ac
midiewozhy <marshal.hy.zhu@gmail.com>
2025-07-21 14:51:19 +0800

    复用飞书client

--- services/client.py
+++ services/client.py
@@ -1,7 +1,15 @@
 from openai import OpenAI
 from config import Config
+import lark_oapi as lark
 
-client = OpenAI(
+llm_client = OpenAI(
     base_url = "https://ark.cn-beijing.volces.com/api/v3/bots",
     api_key = Config.API_KEY
+)
+
+feishu_client = (
+    lark.Client.builder()
+    .enable_set_token(True)
+    .log_level(lark.LogLevel.DEBUG)
+    .build()
 )
