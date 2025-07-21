e64e4ae7ee087b9a130093049be7481b02dc49c1
midiewozhy <marshal.hy.zhu@gmail.com>
2025-07-21 15:21:05 +0800

    由于飞书client的不可复用性，去除feishu_client

--- services/client.py
+++ services/client.py
@@ -1,15 +1,7 @@
 from openai import OpenAI
 from config import Config
-import lark_oapi as lark
 
 llm_client = OpenAI(
     base_url = "https://ark.cn-beijing.volces.com/api/v3/bots",
     api_key = Config.API_KEY
-)
-
-feishu_client = (
-    lark.Client.builder()
-    .enable_set_token(True)
-    .log_level(lark.LogLevel.DEBUG)
-    .build()
 )
