34930d708513af4ae1e9ee3a785fc679edec5014
midiewozhy <marshal.hy.zhu@gmail.com>
2025-07-22 13:52:12 +0800

    存储管理数据

--- services/user_data_manager.py
+++ services/user_data_manager.py
@@ -1,2 +1,14 @@
+from threading import Lock
 class UserDataManger:
-    pass
+    def __init__(self):
+        self._user_data = {}
+        self._set_lock = Lock()
+        self._get_lock = Lock()
+
+    def set_user_data(self, session_id, data):
+        with self._set_lock:
+            self._user_data[session_id] = data
+
+    def get_user_data(self, session_id):
+        with self._get_lock:
+            return self._user_data.get(session_id)
